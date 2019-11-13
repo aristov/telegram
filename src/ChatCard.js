d0cc03aaef1e99e25e08b95dbee11591d162d322
aristov <vv.aristov@gmail.com>
2019-11-14 01:38:52 +0300

    Making ChatCard

--- src/ChatCard.js
+++ src/ChatCard.js
@@ -1,15 +1,53 @@
-import { RoleArticle } from 'ariamodule/lib'
-import { Span } from 'htmlmodule/lib'
+import moment from './moment'
+import { Article, Status } from 'ariamodule/lib'
+import { Div, Span, Time } from 'htmlmodule/lib'
 import { ChatPhoto } from './ChatPhoto'
 import './ChatCard.css'
 
-export class ChatCard extends RoleArticle
+class ChatTitle extends Span
+{
+}
+
+class ChatPreview extends Span
 {
     init(init) {
         super.init(init)
-        this.children = [
-            new ChatPhoto({ chat : init.chat }),
-            new Span(init.chat.title)
+        const content = init.chat.last_message.content
+        const text = content.text? content.text.text : content.caption
+        this.children = text && typeof text.substring === 'function' && text.substring(0, 50)
+    }
+}
+
+class ChatTime extends Time
+{
+}
+
+class ChatNotifier extends Status
+{
+    static get elementAssembler() {
+        return Span
+    }
+}
+
+export class ChatCard extends Article
+{
+    init(init) {
+        super.init(init)
+        this.children = this.build(init)
+    }
+
+    build({ chat }) {
+        const time = chat.last_message.date
+        return [
+            new ChatPhoto({ chat }),
+            new Div([
+                new ChatTitle(chat.title),
+                new ChatTime(moment(time * 1000).format('H:mm'))
+            ]),
+            new Div([
+                new ChatPreview({ chat }),
+                !!chat.unread_count && new ChatNotifier(chat.unread_count)
+            ])
         ]
     }
 }
