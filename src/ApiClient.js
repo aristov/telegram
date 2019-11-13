32ec9b6bff385ae0821c1f1d7dfbad3e57a02646
aristov <vv.aristov@gmail.com>
2019-11-13 20:16:01 +0300

    Handle file downloads

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -1,5 +1,4 @@
 import TdClient from 'tdweb/dist/tdweb'
-import { version } from '../package'
 
 const WASM_FILE_HASH = 'b4b0d61282108a31908dd6b2dbd7067b'
 const WASM_FILE_NAME = WASM_FILE_HASH + '.wasm'
@@ -7,7 +6,8 @@ const API_ID = '1038348'
 const API_HASH = 'c18fa7085ac8a713f3df8ea921cfb760'
 const wasmUrl = `${ WASM_FILE_NAME }?_sw-precache=${ WASM_FILE_HASH }`
 
-export class ApiClient {
+export class ApiClient
+{
     constructor() {
         this._client = new TdClient({ wasmUrl })
         this._client.onUpdate = this.onUpdate.bind(this)
@@ -30,13 +30,20 @@ export class ApiClient {
     }
 
     updateFile(update) {
+        console.log(update)
+        if(update.file.local.is_downloading_completed) {
+            this.readFile(update.file)
+        }
+    }
+
+    readFile(file) {
         this._client.send({
             '@type' : 'readFile',
-            file_id : update.file.id
+            file_id : file.id
         }).then(response => {
             window.dispatchEvent(new CustomEvent('readFile', {
                 detail : {
-                    file : update.file,
+                    file : file,
                     data : response.data
                 }
             }))
@@ -53,7 +60,7 @@ export class ApiClient {
                 system_language_code : navigator.language || 'en',
                 device_model : 'Chrome',
                 system_version : 'Mac/iOS',
-                application_version : version
+                application_version : '1.0.0'
             }
         }).then(console.log).catch(console.error)
     }
@@ -80,36 +87,43 @@ export class ApiClient {
     }
 
     authorizationStateReady(state) {
+        console.log(state)
         this._client.send({
             '@type' : 'getChats',
             offset_order : '9223372036854775807',
             limit : 20
         }).then(response => {
-            if(response['@type'] === 'chats') {
-                Promise.all(response.chat_ids.map(chat_id => {
-                    return this._client.send({
-                        '@type' : 'getChat',
-                        chat_id
-                    })
-                })).then(chats => {
-                    console.log(chats)
-                    window.dispatchEvent(new CustomEvent('ready', { detail : { chats } }))
-                    Promise.all(chats.filter(({ photo }) => photo).map(chat => {
-                        this._client.send({
-                            '@type' : 'downloadFile',
-                            file_id : chat.photo.small.id,
-                            priority : 1
-                        })
-                    })).then(console.log).catch(console.error)
-                    /*this._client.send({
-                        '@type' : 'getChatHistory',
-                        chat_id : chats[0].id,
-                        from_message_id : chats[0].last_message.id,
-                        offset : 0,
-                        limit : 20,
-                    }).then(console.log).catch(console.error)*/
-                }).catch(console.error)
-            }
+            Promise.all(response.chat_ids.map(chat_id => {
+                return this._client.send({
+                    '@type' : 'getChat',
+                    chat_id
+                })
+            })).then(chats => {
+                console.log(chats)
+                window.dispatchEvent(new CustomEvent('ready', { detail : { chats } }))
+                chats.filter(({ photo }) => photo).forEach(chat => {
+                    const local = chat.photo.small.local
+                    if(local.can_be_downloaded) {
+                        if(local.is_downloading_completed) {
+                            this.readFile(chat.photo.small)
+                        }
+                        else if(!local.is_downloading_active) {
+                            this._client.send({
+                                '@type' : 'downloadFile',
+                                file_id : chat.photo.small.id,
+                                priority : 1
+                            }).then(console.log).catch(console.error)
+                        }
+                    }
+                })
+                /*this._client.send({
+                    '@type' : 'getChatHistory',
+                    chat_id : chats[0].id,
+                    from_message_id : chats[0].last_message.id,
+                    offset : 0,
+                    limit : 20,
+                }).then(console.log).catch(console.error)*/
+            }).catch(console.error)
         }).catch(console.error)
     }
 }
